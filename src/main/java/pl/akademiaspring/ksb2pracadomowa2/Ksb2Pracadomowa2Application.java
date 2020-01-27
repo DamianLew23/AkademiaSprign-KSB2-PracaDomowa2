7acad5cc827254e9bbb85f80ddbd4af6f97b97c4
Damian Lew <dlew8912@gmail.com>
2020-01-27 18:07:49 +0100

    Firs Commit

---
+++ src/main/java/pl/akademiaspring/ksb2pracadomowa2/Ksb2Pracadomowa2Application.java
@@ -0,0 +1,13 @@
+package pl.akademiaspring.ksb2pracadomowa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksb2Pracadomowa2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ksb2Pracadomowa2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/akademiaspring/ksb2pracadomowa2/Ksb2Pracadomowa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiaspring.ksb2pracadomowa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksb2Pracadomowa2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
